fc2319e8725bfa1a537035fae3683d8b7fa2fb16
Alumnos DAW <Alumnos DAW@PC-CZC44374RY>
2025-04-04 19:46:57 +0200

    Agregada la clase Calculadora y la funcion resta y clase Ficheros

---
+++ src/LeerFicheros.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Alumnos DAW
+ */
+public class LeerFicheros {
+   private String leerFichero;
+   public LeerFicheros(){
+       
+   }
+   public void leerFicheroXML(){
+       System.out.println("Hola que tal?");
+   }
+   public void leerFicheroTXT(){
+       System.out.println("Leyendo txt");
+   }
+}
